c9d77f66f04a92453a062571c9c0a09f2eb0c209
Piyush Keshan <keshanpiyush112@gmail.com>
2024-11-05 17:41:26 +0530

    updated README

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,14 @@ public class Main {
     public static void main(String[] args) {
         Sales.next_date();
         new Item("Burger", 100, true, List.of("fast food", "mains"));
-        new Item("Pizza", 200, true, List.of("fast food", "mains"));
+        new Item("Pizza", 200, false, List.of("fast food", "mains"));
+        new Item("Pasta", 150, true, List.of("fast food", "sides"));
+        new Item("Coke", 50, true, List.of("drinks"));
+        new Item("Paratha", 50, true, List.of("mains"));
+        new Item("Biryani", 200, false, List.of("mains"));
+        new Item("Toast", 50, false, List.of("breakfast"));
+        new Item("Tea", 50, true, List.of("drinks"));
+        new Item("Coffee", 50, true, List.of("drinks"));
 
         Scanner scanner = new Scanner(System.in);
         try {
@@ -21,8 +28,12 @@ public class Main {
         } catch (LoginFailedError e) {
             System.out.println(e.getMessage());
         }
-        new Customer("a", "a");
+        new Customer ("Aman", "aman@1234");
+        new Customer("Ravi", "ravi@1234");
+        new Customer("Riya", "riya@1234");
+        new Customer("Urvi", "urvi@1234");
 
+        new Customer("a", "a");
         Program.menu(scanner);
     }
 }
